6fb62efc1cdd2036d4245be46336bb02b43c2157
jangyoungdo <mapl003@naver.com>
2017-07-05 14:56:43 +0900

    채팅 공지사항 레이아웃 추가, 리싸이클 뷰로 구현

--- app/src/main/java/com/example/heronation/whatziroom/activity/room/RoomFragment2.java
+++ app/src/main/java/com/example/heronation/whatziroom/activity/room/RoomFragment2.java
@@ -3,6 +3,8 @@ package com.example.heronation.whatziroom.activity.room;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,19 +16,23 @@ import android.widget.TextView;
 
 import com.example.heronation.whatziroom.R;
 import com.example.heronation.whatziroom.adapter.ChatAdapter;
+import com.example.heronation.whatziroom.adapter.ChatNoticeAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ATIV on 2017-06-25.
  */
 
 public class RoomFragment2 extends Fragment {
-    LinearLayout layout;
-
-    ListView m_ListView;
-    ChatAdapter m_Adapter;
-    EditText edChat;
-    TextView sendButton;
-    LinearLayout linChatList;
+    private LinearLayout layout;
+    private ListView m_ListView;
+    private ChatAdapter m_Adapter;
+    private EditText edChat;
+    private TextView sendButton;
+    private LinearLayout linChatList;
+    private RecyclerView recyclerview;
 
     @Nullable
     @Override
@@ -41,6 +47,32 @@ public class RoomFragment2 extends Fragment {
         edChat = (EditText) layout.findViewById(R.id.edChatText);
         sendButton = (TextView) layout.findViewById(R.id.sendChat);
         linChatList = (LinearLayout) layout.findViewById(R.id.linChatList);
+
+        //공지사항에 사용될 리싸이클뷰
+        recyclerview = (RecyclerView) layout.findViewById(R.id.noticeChat);
+        recyclerview.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        List<ChatNoticeAdapter.Item> data = new ArrayList<>();
+
+        data.add(new ChatNoticeAdapter.Item(ChatNoticeAdapter.HEADER, "스케줄을 입력해주세요."));
+        data.add(new ChatNoticeAdapter.Item(ChatNoticeAdapter.CHILD, "밥먹고"));
+        data.add(new ChatNoticeAdapter.Item(ChatNoticeAdapter.CHILD, "술먹고"));
+        data.add(new ChatNoticeAdapter.Item(ChatNoticeAdapter.CHILD, "집고고"));
+//        data.add(new ChatNoticeAdapter.Item(ChatNoticeAdapter.HEADER, "Cars"));
+//        data.add(new ChatNoticeAdapter.Item(ChatNoticeAdapter.CHILD, "Audi"));
+//        data.add(new ChatNoticeAdapter.Item(ChatNoticeAdapter.CHILD, "Aston Martin"));
+//        data.add(new ChatNoticeAdapter.Item(ChatNoticeAdapter.CHILD, "BMW"));
+//        data.add(new ChatNoticeAdapter.Item(ChatNoticeAdapter.CHILD, "Cadillac"));
+
+        // 동적으로 리스트 추가하는 법
+//        ChatNoticeAdapter.Item places = new ChatNoticeAdapter.Item(ChatNoticeAdapter.HEADER, "Places");
+//        places.invisibleChildren = new ArrayList<>();
+//        places.invisibleChildren.add(new ChatNoticeAdapter.Item(ChatNoticeAdapter.CHILD, "Kerala"));
+//        places.invisibleChildren.add(new ChatNoticeAdapter.Item(ChatNoticeAdapter.CHILD, "Tamil Nadu"));
+//        places.invisibleChildren.add(new ChatNoticeAdapter.Item(ChatNoticeAdapter.CHILD, "Karnataka"));
+//        places.invisibleChildren.add(new ChatNoticeAdapter.Item(ChatNoticeAdapter.CHILD, "Maharashtra"));
+
+//        data.add(places);
+        recyclerview.setAdapter(new ChatNoticeAdapter(data));
 
         // ListView에 어댑터 연결
         m_ListView.setAdapter(m_Adapter);
