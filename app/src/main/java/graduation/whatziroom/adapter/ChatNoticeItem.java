7471ac945ca7e267ae52284156a43969dfec4970
Youngdo <mapl003@naver.com>
2017-09-20 11:36:06 +0900

    다음맵 API가 추가가 안되서 프로젝트 새로 생성

---
+++ app/src/main/java/graduation/whatziroom/adapter/ChatNoticeItem.java
@@ -0,0 +1,26 @@
+package graduation.whatziroom.adapter;
+
+
+import net.daum.mf.map.api.MapView;
+
+import java.util.List;
+
+/**
+ * Created by mapl0 on 2017-07-07.
+ */
+
+public class ChatNoticeItem {
+    public int type;
+    public String text;
+    public List<ChatNoticeItem> invisibleChildren;
+    public MapView map;
+
+    public ChatNoticeItem() {
+
+    }
+
+    public ChatNoticeItem(int type, String text) {
+        this.type = type;
+        this.text = text;
+    }
+}
